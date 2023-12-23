f51afc53610bcde078095bd112fb22c69f0c4dea
Aftab1112 <you@example.com>
2023-12-23 16:19:17 +0530

    all about arrays

---
+++ index.js
@@ -0,0 +1,93 @@
+// let student ={
+//     name : "Aftab",
+//     marks : 660,
+//     cgpa : 8.2,
+//     isPass : true,
+// }
+
+// console.log(student)
+
+// let heroes = ["ironman","hulk","spider-man","superman"]
+
+
+// let marks = {
+//     student1 : 97,
+//     student2 : 87
+// }
+
+//let heroes = ["ironman","hulk","thor","batman"];
+// for(i=0;i<heroes.length;i++){
+//     console.log(heroes[i])
+// }
+
+
+// for(let i of heroes){
+//     console.log(i)
+// }
+
+// let cities = ["pune","mumbai","solapur","nashik","hyderabad","bengluru"]
+
+// for(let city of cities){
+//     console.log(city)
+// }
+
+// 
+// let marks = [85,97,44,37,76,60];
+// let sum = 0;
+// let average;
+
+// for(let i of marks){
+//    sum = sum+i
+// }
+
+// console.log(sum)
+
+// average = sum/marks.length;
+// console.log(`${average}`)
+
+// let items = [250,645,300,900,50]
+// let i = 0;
+// for(let val of items){
+//     let offer = val/10
+//     items[i] = items[i] - offer;
+//     console.log(`The value after offer is ${items[i]}`)
+//     i++
+// }
+
+// for(let i=0;i<items.length;i++){
+//     let offer = items[i]/10;
+//     items[i] = items[i] - offer;
+
+// }
+
+// console.log(items)
+
+// let foodItems = ["apple","orange","banana","litchi","orange"]
+// console.log(foodItems)
+// foodItems.pop();
+// console.log(foodItems)
+
+// let marvelHeroes = ["iron-man","spider-man","thor"]
+// let dcHeroes = ["batman","superman","flash"]
+
+// let heroes = marvelHeroes.concat(dcHeroes);
+// console.log(heroes)
+
+// let marks = [1,2,3,4,5,6,7]
+
+// marks.splice(2,2,101,102)
+
+
+// let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"]
+// companies.splice(0,1)
+// companies.splice(1,1,"Ola")
+// companies.push("Amazon")
+
+// console.log(companies)
+
+
+
+
+
+
+
